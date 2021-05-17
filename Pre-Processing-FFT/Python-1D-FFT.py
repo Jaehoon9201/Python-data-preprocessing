4b728b1f1705be8b21e6486eae72a6fdd6099d36
Jae-Hoon Shim <71545160+Jaehoon9201@users.noreply.github.com>
2021-05-17 18:35:35 +0900

    Update Python-1D-FFT.py

--- Pre-Processing-FFT/Python-1D-FFT.py
+++ Pre-Processing-FFT/Python-1D-FFT.py
@@ -18,4 +18,23 @@ plt.plot(t,f_clean,color = 'k', LineWidth = 2, label =  'Clean')
 plt.xlim(t[0], t[-1])
 plt.savefig('plot.png', dpi = 100)
 plt.legend()
+
+n = len(t)
+fhat = np.fft.fft(f,n)
+PSD = fhat * np.conj(fhat)/n
+freq = (1/(dt*n)) * np.arange(n)
+L = np.arange(1,np.floor(n/2), dtype = 'int')
+
+fig, axs = plt.subplots(2,1)
+
+plt.sca(axs[0])
+plt.plot(t,f,color = 'c', LineWidth = 1.5, label = 'Noisy')
+plt.plot(t,f_clean, color = 'k', Linewidth = 2, label = 'Clean')
+plt.xlim(t[0],t[-1])
+plt.legend()
+
+plt.sca(axs[1])
+plt.plot(freq[L],PSD[L], color= 'c', LineWidth = 2, label = 'Noisy')
+plt.xlim(freq[L[0]], freq[L[-1]])
+plt.legend()
 plt.show()
