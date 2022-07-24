2223269e3b1affda648bcf6a6d53cdf95612390d
Jaehoon Shim <71545160+Jaehoon9201@users.noreply.github.com>
2022-07-24 17:49:56 +0900

    Add files via upload

---
+++ Spectrogram_plot/spec_plot.py
@@ -0,0 +1,170 @@
+from pathlib import Path
+import math, random
+import torch
+import torchaudio
+import numpy as np
+from torchaudio import transforms
+
+from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import random_split
+
+import torch.nn.functional as F
+from torch.nn import init
+import torch.nn as nn
+
+from matplotlib import pyplot as plt
+from matplotlib.mlab import window_hanning, specgram
+from matplotlib.colors import LogNorm
+
+
+
+class AudioUtil():
+    # ----------------------------
+    # Load an audio file. Return the signal as a tensor and the sample rate
+    # ----------------------------
+    @staticmethod
+    def open(audio_file):
+        sig, sr = torchaudio.load(audio_file)
+        return (sig, sr)
+
+    # ----------------------------
+    # Since Resample applies to a single channel, we resample one channel at a time
+    # ----------------------------
+    def resample(aud, newsr):
+        sig, sr = aud
+
+        if (sr == newsr):
+            # Nothing to do
+            return aud
+
+        num_channels = sig.shape[0]
+        # Resample first channel
+        resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1, :])
+        if (num_channels > 1):
+            # Resample the second channel and merge both channels
+            retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:, :])
+            resig = torch.cat([resig, retwo])
+
+        return ((resig, newsr))
+
+    # ----------------------------
+    # Convert the given audio to the desired number of channels
+    # ----------------------------
+    @staticmethod
+    def rechannel(aud, new_channel):
+        sig, sr = aud
+
+        if (sig.shape[0] == new_channel):
+            # Nothing to do
+            return aud
+
+        if (new_channel == 1):
+            # Convert from stereo to mono by selecting only the first channel
+            resig = sig[:1, :]
+        else:
+            # Convert from mono to stereo by duplicating the first channel
+            resig = torch.cat([sig, sig])
+
+        return ((resig, sr))
+
+    # ----------------------------
+    # Pad (or truncate) the signal to a fixed length 'max_ms' in milliseconds
+    # ----------------------------
+    @staticmethod
+    def pad_trunc(aud, max_ms):
+        sig, sr = aud
+        num_rows, sig_len = sig.shape
+        max_len = sr // 1000 * max_ms
+
+        if (sig_len > max_len):
+            # Truncate the signal to the given length
+            sig = sig[:, :max_len]
+
+        elif (sig_len < max_len):
+            # Length of padding to add at the beginning and end of the signal
+            pad_begin_len = random.randint(0, max_len - sig_len)
+            pad_end_len = max_len - sig_len - pad_begin_len
+
+            # Pad with 0s
+            pad_begin = torch.zeros((num_rows, pad_begin_len))
+            pad_end = torch.zeros((num_rows, pad_end_len))
+
+            sig = torch.cat((pad_begin, sig, pad_end), 1)
+
+        return (sig, sr)
+
+    # ----------------------------
+    # Shifts the signal to the left or right by some percent. Values at the end
+    # are 'wrapped around' to the start of the transformed signal.
+    # ----------------------------
+    @staticmethod
+    def time_shift(aud, shift_limit):
+        sig, sr = aud
+        _, sig_len = sig.shape
+        shift_amt = int(random.random() * shift_limit * sig_len)
+        return (sig.roll(shift_amt), sr)
+
+    # ----------------------------
+    # Generate a Spectrogram
+    # ----------------------------
+    @staticmethod
+    def spectro_gram(aud, n_mels, n_fft, hop_len):
+        sig = aud
+        sr = 50000
+        top_db = 100
+        spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
+        # spec = transforms.Spectrogram(n_fft=n_fft, hop_length=hop_len)(sig)
+        spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
+
+        return (spec)
+
+    # ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    # ----------------------------
+    # Augment the Spectrogram by masking out some sections of it in both the frequency
+    # dimension (ie. horizontal bars) and the time dimension (vertical bars) to prevent
+    # overfitting and to help the model generalise better. The masked sections are
+    # replaced with the mean value.
+    # ----------------------------
+    @staticmethod
+    def spectro_augment(spec, max_mask_pct, n_freq_masks, n_time_masks):
+        _, n_mels, n_steps = spec.shape
+        mask_value = spec.mean()
+        aug_spec = spec
+
+        freq_mask_param = max_mask_pct * n_mels
+        for _ in range(n_freq_masks):
+            aug_spec = transforms.FrequencyMasking(freq_mask_param)(aug_spec, mask_value)
+
+        time_mask_param = max_mask_pct * n_steps
+        for _ in range(n_time_masks):
+            aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
+
+        return aug_spec
+
+
+def torch2npy(input):
+    input = input.cpu()
+    output = input.detach().numpy()
+    return output
+
+
+
+FREQ = 20000
+# construct a sine wave
+times = np.arange(50000*2).astype(float)
+times = times/50000
+signal = 1 * np.sin(2 * np.pi * times*FREQ )
+plt.plot(times, signal)
+plt.show()
+
+
+signal = torch.from_numpy(np.asarray(signal)).float()
+spec = AudioUtil.spectro_gram(signal, n_mels=128, n_fft=1024, hop_len=512)
+
+
+spec = torch2npy(spec)
+
+[h, w] = spec.shape
+canvas = np.ones((h, w), np.float32)
+
+plt.imsave("%d_Hz_test.png" %(FREQ) , spec)
